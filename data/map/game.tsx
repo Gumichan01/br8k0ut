55d9209a4a68d4429d0a637f662db51d37b4d26c
Gumichan01 <luxon.jean.pierre@gmail.com>
2017-09-02 19:07:05 +0200

    map: first level

---
+++ data/map/game.tsx
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="breakout" tilewidth="8" tileheight="8" tilecount="23" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="8" height="8" source="../image/fond.png"/>
+ </tile>
+ <tile id="1" type="solid">
+  <image width="8" height="8" source="../image/wall.png"/>
+ </tile>
+ <tile id="2" type="solid">
+  <image width="8" height="8" source="../image/gate.png"/>
+ </tile>
+ <tile id="3" type="death">
+  <image width="8" height="8" source="../image/canon.png"/>
+ </tile>
+ <tile id="4" type="death">
+  <image width="8" height="8" source="../image/trap.png"/>
+ </tile>
+ <tile id="5" type="start">
+  <image width="8" height="8" source="../image/start.png"/>
+ </tile>
+ <tile id="6" type="exit">
+  <image width="8" height="8" source="../image/door.png"/>
+ </tile>
+ <tile id="7">
+  <image width="8" height="8" source="../image/letter_a.png"/>
+ </tile>
+ <tile id="8">
+  <image width="8" height="8" source="../image/letter_b.png"/>
+ </tile>
+ <tile id="9">
+  <image width="8" height="8" source="../image/letter_c.png"/>
+ </tile>
+ <tile id="10">
+  <image width="8" height="8" source="../image/letter_g.png"/>
+ </tile>
+ <tile id="11">
+  <image width="8" height="8" source="../image/letter_h.png"/>
+ </tile>
+ <tile id="12">
+  <image width="8" height="8" source="../image/letter_i.png"/>
+ </tile>
+ <tile id="13">
+  <image width="8" height="8" source="../image/letter_k.png"/>
+ </tile>
+ <tile id="14">
+  <image width="8" height="8" source="../image/letter_m.png"/>
+ </tile>
+ <tile id="15">
+  <image width="8" height="8" source="../image/letter_n.png"/>
+ </tile>
+ <tile id="16">
+  <image width="8" height="8" source="../image/letter_r.png"/>
+ </tile>
+ <tile id="17">
+  <image width="8" height="8" source="../image/letter_t.png"/>
+ </tile>
+ <tile id="18">
+  <image width="8" height="8" source="../image/letter_u.png"/>
+ </tile>
+ <tile id="19">
+  <image width="8" height="8" source="../image/letter_y.png"/>
+ </tile>
+ <tile id="20">
+  <image width="8" height="8" source="../image/digit_0.png"/>
+ </tile>
+ <tile id="21">
+  <image width="8" height="8" source="../image/digit_1.png"/>
+ </tile>
+ <tile id="22">
+  <image width="8" height="8" source="../image/digit_8.png"/>
+ </tile>
+</tileset>
