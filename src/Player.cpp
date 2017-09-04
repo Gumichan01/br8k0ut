37292ee3a592fd97db3c4297e3d68fd1a425df3b
Gumichan01 <luxon.jean.pierre@gmail.com>
2017-09-04 13:53:13 +0200

    player: new class

---
+++ src/Player.cpp
@@ -0,0 +1,104 @@
+
+/*
+*   Br8k0ut - Platform Video game
+*   Copyright © 2017 Luxon Jean-Pierre
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   Luxon Jean-Pierre (Gumichan01)
+*   website: https://gumichan01.github.io/
+*   mail: luxon.jean.pierre@gmail.com
+*/
+
+
+#include "Player.hpp"
+
+#include <LunatiX/LX_Hitbox.hpp>
+#include <LunatiX/LX_WindowManager.hpp>
+#include <LunatiX/LX_Graphics.hpp>
+
+
+namespace
+{
+const FloatPosition DFPOS = {0.0f, 0.0f};
+const std::string PLAYER_PATH("./data/image/player.png");
+}
+
+using namespace LX_Win;
+
+/// Floating-point coordinates
+
+FloatPosition::FloatPosition(): x(0.0f), y(0.0f) {}
+FloatPosition::FloatPosition(float fx, float fy): x(fx), y(fy) {}
+FloatPosition::FloatPosition(const FloatPosition& fp): x(fp.x), y(fp.y) {}
+FloatPosition::FloatPosition(const LX_AABB& b): FloatPosition(b.x, b.y) {}
+
+FloatPosition& FloatPosition::operator =(const FloatPosition& fp)
+{
+    x = fp.x;
+    y = fp.y;
+    return *this;
+}
+
+FloatPosition& FloatPosition::operator =(const LX_AABB& aabb)
+{
+    x = aabb.x;
+    y = aabb.y;
+    return *this;
+}
+
+
+FloatPosition& FloatPosition::operator +=(const LX_Physics::LX_Vector2D& v)
+{
+    x += v.vx;
+    y += v.vy;
+    return *this;
+}
+
+void FloatPosition::toPixelUnit(LX_AABB& aabb)
+{
+    aabb.x = static_cast<int>(x);
+    aabb.y = static_cast<int>(y);
+}
+
+
+Player::Player(LX_AABB& pos): sprite(nullptr), fpos(DFPOS), position(pos)
+{
+    LX_Window *win = LX_WindowManager::getInstance()->getWindow(1);
+    sprite = new LX_Graphics::LX_Sprite(PLAYER_PATH, *win, LX_PIXELFORMAT_RGB888);
+    fpos = position;
+}
+
+void Player::draw()
+{
+    sprite->draw(&position);
+}
+
+void Player::move()
+{
+
+}
+
+Player::~Player()
+{
+    delete sprite;
+}
+
+
+
+
+
+
+
+
---
+++ src/Player.hpp
@@ -0,0 +1,76 @@
+
+/*
+*   Br8k0ut - Platform Video game
+*   Copyright © 2017 Luxon Jean-Pierre
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   Luxon Jean-Pierre (Gumichan01)
+*   website: https://gumichan01.github.io/
+*   mail: luxon.jean.pierre@gmail.com
+*/
+
+
+#ifndef PLAYER_HPP_INCLUDED
+#define PLAYER_HPP_INCLUDED
+
+#include <LunatiX/LX_AABB.hpp>
+
+
+namespace LX_Graphics
+{
+class LX_Sprite;
+}
+
+namespace LX_Physics
+{
+struct LX_Circle;
+struct LX_Vector2D;
+}
+
+
+struct FloatPosition
+{
+    float x;
+    float y;
+
+    FloatPosition();
+    FloatPosition(float fx, float fy);
+    FloatPosition(const FloatPosition& fp);
+    explicit FloatPosition(const LX_AABB& b);
+    FloatPosition& operator =(const FloatPosition& fp);
+    FloatPosition& operator =(const LX_AABB& aabb);
+    FloatPosition& operator +=(const LX_Physics::LX_Vector2D& v);
+
+    void toPixelUnit(LX_AABB& aabb);
+};
+
+
+class Player
+{
+    LX_Graphics::LX_Sprite *sprite;
+    FloatPosition fpos;
+    LX_AABB position;
+
+public:
+
+    Player(LX_AABB& pos);
+
+    void draw();
+    void move();
+
+    ~Player();
+};
+
+#endif // PLAYER_HPP_INCLUDED
